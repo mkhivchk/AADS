ceceeb98bf99cdd94a60db138366a58f3b375a04
mkhivchk <105316516+mkhivchk@users.noreply.github.com>
2023-02-02 14:10:35 +0300

    task 3 done

---
+++ homework00/task3/main.py
@@ -0,0 +1,26 @@
+import random as rd
+import time
+
+
+# def fib_func(nums):
+#     if nums <= 1:
+#         return nums
+#     return fib_func(nums - 1) + fib_func(nums - 2)
+
+def fib_func_for(nums):
+    f1 = f2 = 1
+    for _ in range(2, nums):
+        f1, f2 = f2, f1 + f2
+    return f2
+
+
+start_time = time.time()
+
+with open('input.txt', 'w') as start:
+    start.write(str(rd.randint(0, 10 ** 7)))
+
+with open('output.txt', 'w') as result:
+    with open('input.txt') as num:
+        result.write(str(fib_func_for(int(num.read()) % 10)))
+
+print(f'---- {time.time() - start_time} seconds ----')

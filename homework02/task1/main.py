8070dfbcbf4530f2042cb61e3132a83b19b3917b
m.ivchik <misas20202@gmail.com>
2023-08-26 00:53:08 +0300

    homework 2 task 1

---
+++ homework02/task1/main.py
@@ -0,0 +1,51 @@
+import random
+
+
+def merge_sort(array: list):
+    # Делим массив пополам
+    if len(array) > 1:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+
+    # Рекурсивно вызываем тот же самый мердж сорт, делим разделенный масив еще раз
+        merge_sort(left)
+        merge_sort(right)
+
+    # Заканчиваем делить на моменте, когда массив будет <= 1
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+
+
+with open('input.txt', 'w') as input:
+    n = random.randint(1, 200000)
+    input.write(str(n) + '\n')
+    massive = [random.randint(-1000000000, 1000000000) for _ in range(n)]
+    for el in massive:
+        input.write(str(el) + ' ')
+
+with open('input.txt', 'r') as ini, open('output.txt', 'w') as out:
+    data: list = ini.readlines()[1].split(' ')
+    merge_sort(data)
+    for nums in data:
+        out.write(nums + ' ')
